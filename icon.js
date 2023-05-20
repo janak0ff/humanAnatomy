8743b039ec8f7654020a9c6d207dd0cd8cc010bc
Janak kr shrestha <janaks2056@gmail.com>
2023-05-20 14:59:33 +0545

    vide

---
+++ icon.js
@@ -0,0 +1,146 @@
+// ========================== vid address ====================
+// var video_idname = "1xYLyOb8fddfsgsfgxxh1QG/Acne";
+// var vdorul = "https://example.com/e/" + video_idname + ".mp4";
+// document.getElementById("acnevdo").setAttribute("href", vdorul);
+// // ------------------------------
+
+// function addHref() {
+// 	var link = document.getElementById("myLink");
+// 	link.setAttribute("href", "https://www.example.com");
+// }
+
+//     id="aAdvancedLipidTesting" onclick="aAdvancedLipidTesting()"
+
+// uncategory
+// const cardiacevents = () => {
+//     let link = document.getElementById("cardiacevents");
+//     link.setAttribute("href", "https://short.ink/V6yVLDZMKAtKlX7");
+//     setTimeout(() => {
+//         link.removeAttribute("href");
+//     }, 1000);
+// };
+// const DPPInhibitors = () => {
+//     let link = document.getElementById("DPPInhibitors");
+//     link.setAttribute("href", "https://short.ink/MPPar6G2Adh6lK");
+//     setTimeout(() => {
+//         link.removeAttribute("href");
+//     }, 1000);
+// };
+
+
+// fetch('file.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     const url = data.Oncology.BreastCancer;
+//     console.log(url);
+//   })
+//   .catch(error => console.error(error));
+
+
+
+
+const setLink = (id, url) => {
+  const link = document.getElementById(id);
+  link.setAttribute("href", url);
+  setTimeout(() => link.removeAttribute("href"), 1000);
+};
+//  Cardiovascular Medicine
+const aAdvancedLipidTesting = () => setLink("aAdvancedLipidTesting", "https://short.ink/j_rla-SyUC");
+const MyocardialInfarction = () => setLink("MyocardialInfarction", "https://short.ink/bvgkldJKdvhPV4M");
+const Atherosclerosis = () => setLink("Atherosclerosis", "https://short.ink/xOrkMaqjD9hkLe7");
+const Cholesterol = () => setLink("Cholesterol", "https://short.ink/DG6Xb32rz8fkO4X");
+const ComplicationsofHypertension = () => setLink("ComplicationsofHypertension", "https://short.ink/a1vPG9OZz4uxwgd");
+const CABG = () => setLink("CABG", "https://short.ink/O6x9Oza09vHm8v");
+const Dyslipidemia = () => setLink("Dyslipidemia", "https://short.ink/k3w4bbQkA73TOvq6");
+const Hypertension = () => setLink("Hypertension", "https://short.ink/yoxmXylYa2c1AvQ");
+const PTCA = () => setLink("PTCA", "https://short.ink/W8re6RpmvJhAAp");
+const RegulationofBloodPressure = () => setLink("RegulationofBloodPressure", "https://short.ink/kgBDwezovYcOl4B");
+const StableAngina = () => setLink("StableAngina", "https://short.ink/y27aR9b9Bmh1V2l");
+
+// Dermatology
+const Acne = () => setLink("Acne", "https://short.ink/1xYLyOb8xxh1QG");
+const Dandruff = () => setLink("Dandruff", "https://short.ink/K0oaY3WBqPuaAj");
+const Psoriasis = () => setLink("Psoriasis", "https://short.ink/l0yv2yz1JXIdwW");
+
+// Gastroenterology
+const alcoholicLiverDisease = () => setLink("alcoholicLiverDisease", "https://short.ink/vQzrWA78O3cD31");
+const GERD = () => setLink("GERD", "https://short.ink/3q7oY00wOVclQb");
+const Inflammatoryboweldisease = () => setLink("Inflammatoryboweldisease", "https://short.ink/7XkWgVjJ99uAXwq");
+const IrritableBowelSyndrome = () => setLink("IrritableBowelSyndrome", "https://short.ink/OaqGdVAZxjcZq9K");
+
+// Endocrinology
+const DiabeticNeuropathy = () => setLink("DiabeticNeuropathy", "https://short.ink/bQw83or1jpIP8dX");
+const Diabeticretinopathy = () => setLink("Diabeticretinopathy", "https://short.ink/8dGGl8kWDvtl30");
+const Insulinresistance = () => setLink("Insulinresistance", "https://short.ink/Ly90BADp9lHvYm");
+const Metabolicsyndrome = () => setLink("Metabolicsyndrome", "https://short.ink/1J7WMrZqmzuev1e");
+const typetwodiabetes = () => setLink("typetwodiabetes", "https://short.ink/ogeevkw3B0hJqPA");
+const obesity = () => setLink("obesity", "https://short.ink/RpXbMjKoajfde6A");
+const diabetes = () => setLink("diabetes", "https://short.ink/YG9ee6GRLzI6x3");
+
+// Essential Atlas of human anatomy and psychology
+const IntroductiontoCell = () => setLink("IntroductiontoCell", "https://short.ink/23yBPOWomPSAll");
+const circulatorysystem = () => setLink("circulatorysystem", "https://short.ink/qxYXMXz8e7iz0OW");
+const DigestiveandExcretorysystem = () => setLink("DigestiveandExcretorysystem", "https://short.ink/Vm60MYKL0KSKM9D");
+const Endocrinesystem = () => setLink("Endocrinesystem", "https://short.ink/kqWgMekagRf9lj");
+const immunesystem = () => setLink("immunesystem", "https://short.ink/xgawDJBGkQHkK4y");
+const integumentarysystem = () => setLink("integumentarysystem", "https://short.ink/aqo96aqwPbHx6W2");
+const Muscularsystem = () => setLink("Muscularsystem", "https://short.ink/oGGaaQ0xadtJ4oJ");
+const Nervoussystem = () => setLink("Nervoussystem", "https://short.ink/Xxv6O8VvdJSDrMM");
+const Reproductivesystem = () => setLink("Reproductivesystem", "https://short.ink/1bx3eVbg3xFx4a");
+const Respiratorysystem = () => setLink("Respiratorysystem", "https://short.ink/lGwBYobOlXC7Ljg");
+const Skeletalsystem = () => setLink("Skeletalsystem", "https://short.ink/kZpkv8o16ohOzbk");
+
+// Gynecology
+const menopause = () => setLink("menopause", "https://short.ink/ZVaYxRM9jGHMv1");
+const PolycysticOvarianSyndrome = () => setLink("PolycysticOvarianSyndrome", "https://short.ink/Z1JJ0qq6oMtq0Rp");
+
+// Neurology
+const AlzheimerDisease = () => setLink("AlzheimerDisease", "https://short.ink/qjBX8aq2zmtA0X");
+const Epilepsy = () => setLink("Epilepsy", "https://short.ink/YK2K32oLqZUvWOM");
+const Migraine = () => setLink("Migraine", "https://short.ink/mDKXOXAz0xtb8y0");
+const neuropathicpain = () => setLink("neuropathicpain", "https://short.ink/w7kA49xyDpHjKy");
+const parkinsonsdisease = () => setLink("parkinsonsdisease", "https://short.ink/060raYG6jjIb7zb");
+const pneumococcalmeningitis = () => setLink("pneumococcalmeningitis", "https://short.ink/g9bbg99KPmHq630");
+const spasticity = () => setLink("spasticity", "https://short.ink/r9QJDKPL2qsbRRy");
+const stroke = () => setLink("stroke", "https://short.ink/Lk1vXby4bzcW6D");
+
+// Oncology
+const breastcancer = () => setLink("breastcancer", "https://short.ink/9yZGqM1ozqiaQJD");
+const prostatecancer = () => setLink("prostatecancer", "https://short.ink/kDVQdXLbKDuD0r");
+
+// Ophthalmology
+const Glaucoma = () => setLink("Glaucoma", "https://short.ink/e27x21wVr3coYp");
+const LASIKandCataract = () => setLink("LASIKandCataract", "https://short.ink/P9LYypAwrMS0p1d");
+
+// Orthopaedics
+const Osteoarthritis = () => setLink("Osteoarthritis", "https://short.ink/eVg6mYozMMIYl86");
+const osteoporosis = () => setLink("osteoporosis", "https://short.ink/qM3YBP6BdPfzlJq");
+const postmenopausalosteoporosis = () => setLink("postmenopausalosteoporosis", "https://short.ink/g2W44mboB4UqyGB");
+const psoriaticarthritis = () => setLink("psoriaticarthritis", "https://short.ink/3opBzJMpdjCdy3D");
+
+
+// Otolaryngology
+const otitismedia = () => setLink("otitismedia", "https://short.ink/D9L3e7Jwgbtkga0");
+const sinusitis = () => setLink("sinusitis", "https://short.ink/QrOz4dvKyZtkvj");
+
+// Psychiatry
+const ADHDinChildren = () => setLink("ADHDinChildren", "https://short.ink/Y6Bz2KW6xzsvPOP");
+const Schizophrenia = () => setLink("Schizophrenia", "https://short.ink/Y7bJga8kkWhvkQk");
+const Depression = () => setLink("Depression", "https://short.ink/ldl0mOb6A4izA2");
+const BipolarDisorder = () => setLink("BipolarDisorder", "https://short.ink/mvyLQpBpd0FbjJ7");
+
+// Respiratory diseases
+const AllergicRhinitis = () => setLink("AllergicRhinitis", "https://short.ink/6mmLvXOeyMIeKJ");
+const ChronicAsthma = () => setLink("ChronicAsthma", "https://short.ink/pLQZqKkdyDurgkW");
+const COPD = () => setLink("COPD", "https://short.ink/eKmqBObvDZcYDpv");
+const upperRespiratorytrack = () => setLink("upperRespiratorytrack", "https://short.ink/wO4OZywkabCJv9K");
+
+// Urology_and_Infectious_Disease
+const BenignProstaticHyperplasia = () => setLink("BenignProstaticHyperplasia", "https://short.ink/mDV6kAm1krTXlP");
+const hivandaids = () => setLink("hivandaids", "https://short.ink/0BW01mPyZaHbPPL");
+
+// uncategory
+const cardiacevents = () => setLink("cardiacevents", "https://short.ink/V6yVLDZMKAtKlX7");
+const DPPInhibitors = () => setLink("DPPInhibitors", "https://short.ink/MPPar6G2Adh6lK");
+
+
