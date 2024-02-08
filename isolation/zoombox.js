1c538ac6c1bd98da12aa4beff0da478646d834ae
Janak kr shrestha <janakkrshrestha@gmail.com>
2024-02-08 15:50:31 +0545

    video link update

--- isolation/zoombox.js
+++ isolation/zoombox.js
@@ -4,103 +4,103 @@ const setLink = (id, url) => {
     setTimeout(() => link.removeAttribute("href"), 1000);
   };
   //  Cardiovascular Medicine
-  const aAdvancedLipidTesting = () => setLink("aAdvancedLipidTesting", "https://short.ink/j_rla-SyUC");
-  const MyocardialInfarction = () => setLink("MyocardialInfarction", "https://short.ink/nqk8uGZ1n");
-  const Atherosclerosis = () => setLink("Atherosclerosis", "https://short.ink/stXILZIow");
-  const Cholesterol = () => setLink("Cholesterol", "https://short.ink/B2SlCFD4o");
-  const ComplicationsofHypertension = () => setLink("ComplicationsofHypertension", "https://short.ink/zyKoxcSgH");
-  const CABG = () => setLink("CABG", "https://short.ink/DUWv-rgFS");
-  const Dyslipidemia = () => setLink("Dyslipidemia", "https://short.ink/kC-9o2-og");
-  const Hypertension = () => setLink("Hypertension", "https://short.ink/_QvrhHlS7");
-  const PTCA = () => setLink("PTCA", "https://short.ink/zwHg4KJCm");
-  const RegulationofBloodPressure = () => setLink("RegulationofBloodPressure", "https://short.ink/WLSnhJdOE");
-  const StableAngina = () => setLink("StableAngina", "https://short.ink/_Wi8Ncb1t");
+  const aAdvancedLipidTesting = () => setLink("aAdvancedLipidTesting", "https://rumble.com/embed/v2ngt72/?pub=1qpfff");
+  const MyocardialInfarction = () => setLink("MyocardialInfarction", "https://rumble.com/embed/v2p3zq4/?pub=1qpfff");
+  const Atherosclerosis = () => setLink("Atherosclerosis", "https://rumble.com/embed/v2ngu7m/?pub=1qpfff");
+  const Cholesterol = () => setLink("Cholesterol", "https://rumble.com/embed/v2ngy9a/?pub=1qpfff");
+  const ComplicationsofHypertension = () => setLink("ComplicationsofHypertension", "https://rumble.com/embed/v2nh3se/?pub=1qpfff");
+  const CABG = () => setLink("CABG", "https://rumble.com/embed/v2nh5l6/?pub=1qpfff");
+  const Dyslipidemia = () => setLink("Dyslipidemia", "https://rumble.com/embed/v2oazb3/?pub=1qpfff");
+  const Hypertension = () => setLink("Hypertension", "https://rumble.com/embed/v2p3v40/?pub=1qpfff");
+  const PTCA = () => setLink("PTCA", "https://rumble.com/embed/v2p45k4/?pub=1qpfff");
+  const RegulationofBloodPressure = () => setLink("RegulationofBloodPressure", "https://rumble.com/embed/v2p45u8/?pub=1qpfff");
+  const StableAngina = () => setLink("StableAngina", "https://rumble.com/embed/v2p46z2/?pub=1qpfff");
   
   // Dermatology
-  const Acne = () => setLink("Acne", "https://short.ink/K8WEFx2g1");
-  const Dandruff = () => setLink("Dandruff", "https://short.ink/WqXOlVAdk");
-  const Psoriasis = () => setLink("Psoriasis", "https://short.ink/wxrtWep-z");
+  const Acne = () => setLink("Acne", "https://rumble.com/embed/v2ngpnw/?pub=1qpfff");
+  const Dandruff = () => setLink("Dandruff", "https://rumble.com/embed/v2nh6dk/?pub=1qpfff");
+  const Psoriasis = () => setLink("Psoriasis", "https://rumble.com/embed/v2p43y3/?pub=1qpfff");
   
   // Gastroenterology
-  const alcoholicLiverDisease = () => setLink("alcoholicLiverDisease", "https://short.ink/WpZdzEbxr");
-  const GERD = () => setLink("GERD", "https://short.ink/-k_qc8LSj");
-  const Inflammatoryboweldisease = () => setLink("Inflammatoryboweldisease", "https://short.ink/yNmk5HXEU");
-  const IrritableBowelSyndrome = () => setLink("IrritableBowelSyndrome", "https://short.ink/AizHJSvIk");
+  const alcoholicLiverDisease = () => setLink("alcoholicLiverDisease", "https://rumble.com/embed/v2ngt9e/?pub=1qpfff");
+  const GERD = () => setLink("GERD", "https://rumble.com/embed/v2ob01m/?pub=1qpfff");
+  const Inflammatoryboweldisease = () => setLink("Inflammatoryboweldisease", "https://rumble.com/embed/v2p3vmo/?pub=1qpfff");
+  const IrritableBowelSyndrome = () => setLink("IrritableBowelSyndrome", "https://rumble.com/embed/v2p3wzq/?pub=1qpfff");
   
   // Endocrinology
-  const DiabeticNeuropathy = () => setLink("DiabeticNeuropathy", "https://short.ink/EAJS--tok");
-  const Diabeticretinopathy = () => setLink("Diabeticretinopathy", "https://short.ink/q__BcFFht");
-  const Insulinresistance = () => setLink("Insulinresistance", "https://short.ink/mvyTQIx9F");
-  const Metabolicsyndrome = () => setLink("Metabolicsyndrome", "https://short.ink/MjfDbSmk4");
-  const typetwodiabetes = () => setLink("typetwodiabetes", "https://short.ink/1JrwD9imO");
-  const obesity = () => setLink("obesity", "https://short.ink/pCDA2SA_qw");
-  const diabetes = () => setLink("diabetes", "https://short.ink/fQ47mAI5xh");
+  const DiabeticNeuropathy = () => setLink("DiabeticNeuropathy", "https://rumble.com/embed/v2oaqdw/?pub=1qpfff");
+  const Diabeticretinopathy = () => setLink("Diabeticretinopathy", "https://rumble.com/embed/v2oav0q/?pub=1qpfff");
+  const Insulinresistance = () => setLink("Insulinresistance", "https://rumble.com/embed/v2p3whu/?pub=1qpfff");
+  const Metabolicsyndrome = () => setLink("Metabolicsyndrome", "https://rumble.com/embed/v2p3ykw/?pub=1qpfff");
+  const typetwodiabetes = () => setLink("typetwodiabetes", "https://rumble.com/embed/v2p47jo/?pub=1qpfff");
+  const obesity = () => setLink("obesity", "https://rumble.com/embed/v2p40v4/?pub=1qpfff");
+  const diabetes = () => setLink("diabetes", "https://rumble.com/embed/v2oap7w/?pub=1qpfff");
   
   // Essential Atlas of human anatomy and psychology
   const IntroductiontoCell = () => setLink("IntroductiontoCell", "https://rumble.com/embed/v2rqyga/?pub=1qpfff");
-  const circulatorysystem = () => setLink("circulatorysystem", "https://short.ink/SM2zjhgW-");
-  const DigestiveandExcretorysystem = () => setLink("DigestiveandExcretorysystem", "https://short.ink/72oqch9RR");
-  const Endocrinesystem = () => setLink("Endocrinesystem", "https://short.ink/uqc3TJdtE");
-  const immunesystem = () => setLink("immunesystem", "https://short.ink/eJEfVq0EF");
-  const integumentarysystem = () => setLink("integumentarysystem", "https://short.ink/Sc9-4IoVP");
-  const Muscularsystem = () => setLink("Muscularsystem", "https://short.ink/EeavWsH8h");
-  const Nervoussystem = () => setLink("Nervoussystem", "https://short.ink/wRWrkQ2SRA");
-  const Reproductivesystem = () => setLink("Reproductivesystem", "https://short.ink/QBfuWpBNY");
-  const Respiratorysystem = () => setLink("Respiratorysystem", "https://short.ink/1zPpL5-h7");
-  const Skeletalsystem = () => setLink("Skeletalsystem", "https://short.ink/vIeYUrwV3");
+  const circulatorysystem = () => setLink("circulatorysystem", "https://rumble.com/embed/v2rqwuc/?pub=1qpfff");
+  const DigestiveandExcretorysystem = () => setLink("DigestiveandExcretorysystem", "https://rumble.com/embed/v2rqx7y/?pub=1qpfff");
+  const Endocrinesystem = () => setLink("Endocrinesystem", "https://rumble.com/embed/v2rqxha/?pub=1qpfff");
+  const immunesystem = () => setLink("immunesystem", "https://rumble.com/embed/v2rqxgi/?pub=1qpfff");
+  const integumentarysystem = () => setLink("integumentarysystem", "https://rumble.com/embed/v2rqxl6/?pub=1qpfff");
+  const Muscularsystem = () => setLink("Muscularsystem", "https://rumble.com/embed/v2rqydy/?pub=1qpfff");
+  const Nervoussystem = () => setLink("Nervoussystem", "https://rumble.com/embed/v2rqyf4/?pub=1qpfff");
+  const Reproductivesystem = () => setLink("Reproductivesystem", "https://rumble.com/embed/v2rqyr6/?pub=1qpfff");
+  const Respiratorysystem = () => setLink("Respiratorysystem", "https://rumble.com/embed/v2rqyuo/?pub=1qpfff");
+  const Skeletalsystem = () => setLink("Skeletalsystem", "https://rumble.com/embed/v2rqyxs/?pub=1qpfff");
   
   // Gynecology
-  const menopause = () => setLink("menopause", "https://short.ink/kIGNTI17f");
-  const PolycysticOvarianSyndrome = () => setLink("PolycysticOvarianSyndrome", "https://short.ink/48D09ivAI");
+  const menopause = () => setLink("menopause", "https://rumble.com/embed/v2p3xsw/?pub=1qpfff");
+  const PolycysticOvarianSyndrome = () => setLink("PolycysticOvarianSyndrome", "https://rumble.com/embed/v2p42tq/?pub=1qpfff");
   
   // Neurology
-  const AlzheimerDisease = () => setLink("AlzheimerDisease", "https://short.ink/OrA0-R09y");
-  const Epilepsy = () => setLink("Epilepsy", "https://short.ink/gccS7LIyp");
-  const Migraine = () => setLink("Migraine", "https://short.ink/rVz9U7h3q");
-  const neuropathicpain = () => setLink("neuropathicpain", "https://short.ink/_36UiUpbE");
-  const parkinsonsdisease = () => setLink("parkinsonsdisease", "https://short.ink/4R0Bl801o");
-  const pneumococcalmeningitis = () => setLink("pneumococcalmeningitis", "https://short.ink/uhuUetU38");
-  const spasticity = () => setLink("spasticity", "https://short.ink/KhOWEhfI1");
-  const stroke = () => setLink("stroke", "https://short.ink/bRIWavPY5");
+  const AlzheimerDisease = () => setLink("AlzheimerDisease", "https://rumble.com/embed/v2ngu5o/?pub=1qpfff");
+  const Epilepsy = () => setLink("Epilepsy", "https://rumble.com/embed/v2oazm2/?pub=1qpfff");
+  const Migraine = () => setLink("Migraine", "https://rumble.com/embed/v2p3z54/?pub=1qpfff");
+  const neuropathicpain = () => setLink("neuropathicpain", "https://rumble.com/embed/v2p40e0/?pub=1qpfff");
+  const parkinsonsdisease = () => setLink("parkinsonsdisease", "https://rumble.com/embed/v2p42i2/?pub=1qpfff");
+  const pneumococcalmeningitis = () => setLink("pneumococcalmeningitis", "https://rumble.com/embed/v2p42zk/?pub=1qpfff");
+  const spasticity = () => setLink("spasticity", "https://rumble.com/embed/v2p4708/?pub=1qpfff");
+  const stroke = () => setLink("stroke", "https://rumble.com/embed/v2p472k/?pub=1qpfff");
   
   // Oncology
-  const breastcancer = () => setLink("breastcancer", "https://short.ink/zVRbiOZH_");
-  const prostatecancer = () => setLink("prostatecancer", "https://short.ink/WxXZReZfw");
+  const breastcancer = () => setLink("breastcancer", "https://rumble.com/embed/v2ngyag/?pub=1qpfff");
+  const prostatecancer = () => setLink("prostatecancer", "https://rumble.com/embed/v2p4456/?pub=1qpfff");
   
   // Ophthalmology
-  const Glaucoma = () => setLink("Glaucoma", "https://short.ink/FlREB0Sbx");
-  const LASIKandCataract = () => setLink("LASIKandCataract", "https://short.ink/AEC1At-49");
+  const Glaucoma = () => setLink("Glaucoma", "https://rumble.com/embed/v2ob0hk/?pub=1qpfff");
+  const LASIKandCataract = () => setLink("LASIKandCataract", "https://rumble.com/embed/v2p3xgg/?pub=1qpfff");
   
   // Orthopaedics
-  const Osteoarthritis = () => setLink("Osteoarthritis", "https://short.ink/6y8xbt73-");
-  const osteoporosis = () => setLink("osteoporosis", "https://short.ink/AXoANfkA6");
-  const postmenopausalosteoporosis = () => setLink("postmenopausalosteoporosis", "https://short.ink/kDGzCJw_k");
-  const psoriaticarthritis = () => setLink("psoriaticarthritis", "https://short.ink/7om-jCO2s");
+  const Osteoarthritis = () => setLink("Osteoarthritis", "https://rumble.com/embed/v2p41bg/?pub=1qpfff");
+  const osteoporosis = () => setLink("osteoporosis", "https://rumble.com/embed/v2p41ig/?pub=1qpfff");
+  const postmenopausalosteoporosis = () => setLink("postmenopausalosteoporosis", "https://rumble.com/embed/v2p43z0/?pub=1qpfff");
+  const psoriaticarthritis = () => setLink("psoriaticarthritis", "https://rumble.com/embed/v2p4528/?pub=1qpfff");
   
   
   // Otolaryngology
-  const otitismedia = () => setLink("otitismedia", "https://short.ink/WucowIAf0");
-  const sinusitis = () => setLink("sinusitis", "https://short.ink/85u-sYJ_L");
+  const otitismedia = () => setLink("otitismedia", "https://rumble.com/embed/v2p424u/?pub=1qpfff");
+  const sinusitis = () => setLink("sinusitis", "https://rumble.com/embed/v2p46do/?pub=1qpfff");
   
   // Psychiatry
-  const ADHDinChildren = () => setLink("ADHDinChildren", "https://short.ink/K8WEFx2g1");
-  const Schizophrenia = () => setLink("Schizophrenia", "https://short.ink/I1XJ78-gQ");
-  const Depression = () => setLink("Depression", "https://short.ink/tG_GR3D_m");
-  const BipolarDisorder = () => setLink("BipolarDisorder", "https://short.ink/aXloeRtli");
+  const ADHDinChildren = () => setLink("ADHDinChildren", "https://rumble.com/embed/v2ngpw2/?pub=1qpfff");
+  const Schizophrenia = () => setLink("Schizophrenia", "https://rumble.com/embed/v2p45yi/?pub=1qpfff");
+  const Depression = () => setLink("Depression", "https://rumble.com/embed/v2nh882/?pub=1qpfff");
+  const BipolarDisorder = () => setLink("BipolarDisorder", "https://rumble.com/embed/v2ngv2c/?pub=1qpfff");
   
   // Respiratory diseases
-  const AllergicRhinitis = () => setLink("AllergicRhinitis", "https://short.ink/8Kz-qOenz");
-  const ChronicAsthma = () => setLink("ChronicAsthma", "https://short.ink/F6zJobKwb");
-  const COPD = () => setLink("COPD", "https://short.ink/5nyBjV7W9P");
-  const upperRespiratorytrack = () => setLink("upperRespiratorytrack", "https://short.ink/LxZqpoAPz");
+  const AllergicRhinitis = () => setLink("AllergicRhinitis", "https://rumble.com/embed/v2ngtic/?pub=1qpfff");
+  const ChronicAsthma = () => setLink("ChronicAsthma", "https://rumble.com/embed/v2nh4k0/?pub=1qpfff");
+  const COPD = () => setLink("COPD", "https://rumble.com/embed/v2nh48c/?pub=1qpfff");
+  const upperRespiratorytrack = () => setLink("upperRespiratorytrack", "https://rumble.com/embed/v2p47oc/?pub=1qpfff");
   
   // Urology_and_Infectious_Disease
-  const BenignProstaticHyperplasia = () => setLink("BenignProstaticHyperplasia", "https://short.ink/C2gT0fKbY");
-  const hivandaids = () => setLink("hivandaids", "https://short.ink/LAbvw-rb1");
+  const BenignProstaticHyperplasia = () => setLink("BenignProstaticHyperplasia", "https://rumble.com/embed/v2ngun6/?pub=1qpfff");
+  const hivandaids = () => setLink("hivandaids", "https://rumble.com/embed/v2ob29o/?pub=1qpfff");
   
   // uncategory
-  const cardiacevents = () => setLink("cardiacevents", "https://short.ink/O6mWbzWMh");
-  const DPPInhibitors = () => setLink("DPPInhibitors", "https://short.ink/QUuLZddNL");
+  const cardiacevents = () => setLink("cardiacevents", "https://rumble.com/embed/v2ngvs8/?pub=1qpfff");
+  const DPPInhibitors = () => setLink("DPPInhibitors", "https://rumble.com/embed/v2oauzy/?pub=1qpfff");
   
   
   
